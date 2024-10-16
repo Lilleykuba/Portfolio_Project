875b4f4ff3d8a5316b74e426a9cd2c2062253192
Camobeast <jakub.lilley@gmail.com>
2024-10-16 10:41:11 +0200

    bug fix #2

--- my-project/src/routes/+layout.svelte
+++ my-project/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
 
   let y;
   let innerHeight = 0;
