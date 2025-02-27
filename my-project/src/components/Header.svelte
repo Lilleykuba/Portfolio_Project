0ecbf4373b0f73caa22c3651a5aed7a1982d30e3
Camobeast <jakub.lilley@gmail.com>
2025-02-27 10:22:40 +0100

    lang switch button width adjustment

--- my-project/src/components/Header.svelte
+++ my-project/src/components/Header.svelte
@@ -49,7 +49,7 @@
         LinkedIn
     </a>
     <div class="flex items-center gap-4">
-        <button on:click={toggleLanguage} class="bg-violet-400 text-white px-4 py-2 ml-4 rounded">
+        <button on:click={toggleLanguage} class="bg-violet-400 text-white px-4 py-2 ml-4 rounded max-w-[50px] sm:max-w-auto">
             {language === 'en' ? 'CZ - Pro klienty' : 'EN - For hiring managers'}
         </button>
     </div>   
