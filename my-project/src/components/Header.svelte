4246471c9e246110a567408fa79262ce84492fe7
Camobeast <jakub.lilley@gmail.com>
2025-02-27 10:29:11 +0100

    lang button styling

--- my-project/src/components/Header.svelte
+++ my-project/src/components/Header.svelte
@@ -49,7 +49,7 @@
         LinkedIn
     </a>
     <div class="flex items-center gap-4">
-        <button on:click={toggleLanguage} class="bg-violet-400 text-white px-4 py-2 ml-4 rounded max-w-[50px] sm:max-w-auto">
+        <button on:click={toggleLanguage} class="bg-violet-400 text-white text-sm px-4 py-2 ml-4 rounded max-w-[100px]">
             {language === 'en' ? 'CZ - Pro klienty' : 'EN - For hiring managers'}
         </button>
     </div>   
